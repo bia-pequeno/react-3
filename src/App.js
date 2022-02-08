e4c14e5607aa46bd17b5c14866ee31ea4d78897d
bia-pequeno <bia.2017pequeno@gmail.com>
2022-02-08 16:56:16 -0300

    concluido

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+ import React, {Component} from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class menssage extends Component{
+  state = {
+    nome: "Beatriz",
+    idade: 17,
+    comidafavorita: "pizza",
+    musicas: [
+      {
+        musica1: "moment - Vierre Clound"
+      },
+      {
+        musica2: "Lover Of Mine - 5 Seconds of Summer"
+      },
+      {
+        musica3: "Whisky a Go-Go - Roupa nova"
+      }
+    ]
+  }
+  render(){
+    return(
+      <div>
+        <h1>Meu nome é {this.state.nome}</h1>
+        <h2>tenho {this.state.idade} anos</h2>
+        <h3>e minha comida favorita é {this.state.comidafavorita}</h3>
+        <ul>
+          <li>{this.state.musicas[0].musica1}</li>
+          <li>{this.state.musicas[1].musica2}</li>
+          <li>{this.state.musicas[2].musica3}</li>
+        </ul>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default menssage
